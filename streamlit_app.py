eda7a8095af2c064ae00a21f3305a24820617d99
jookaren926 <117871270+jookaren926@users.noreply.github.com>
2023-09-26 01:30:49 +0000

    publish 3 - unused import remove

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
